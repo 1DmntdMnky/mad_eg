f9d8db3b022351ddeb8a2a9255204cd8c4b86559
1DmntdMnky <preetjha700@gmail.com>
2022-08-22 14:19:49 +0530

    added list view

--- app/src/main/java/com/example/test/MainActivity2.java
+++ app/src/main/java/com/example/test/MainActivity2.java
@@ -3,6 +3,9 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        String arr[]={"Mumbai", "Daman", "Surat"};
+        ListAdapter ls= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        ListView iv=(ListView) findViewById(R.id.listview);
+        iv.setAdapter(ls);
     }
+
+
 }
