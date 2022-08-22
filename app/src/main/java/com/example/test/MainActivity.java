d2a90ba59913c95ba0a55166de9db4a26e8f33fd
1DmntdMnky <preetjha700@gmail.com>
2022-08-22 13:39:20 +0530

    first commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+EditText ed1, ed2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void convert(View v){
+        ed1=(EditText)findViewById(R.id.id_length);
+        ed2=(EditText) findViewById(R.id.id_answer);
+
+        float num=Float.parseFloat(ed1.getText().toString());
+        float converted_value=num*100;
+        ed2.setText(Float.toString(converted_value)+"cm");
+        if(converted_value>100){
+            Toast toast=Toast.makeText(getApplicationContext(),"length>100cm",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
